43cc7a3112c5ab7d63e7bbc494179c35ef700462
Ignacio Ibarra <ignacio@ignacio.(none)>
2014-05-30 10:31:40 -0400

    second commit

---
+++ src/CalculadoraDigital.java
@@ -0,0 +1,34 @@
+
+public class CalculadoraDigital implements CalculatorInterface {
+
+	@Override
+	public void add(double x, double y) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void subtract(double x, double y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void multiply(double x, double y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void divide(double x, double y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getResult() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+}
